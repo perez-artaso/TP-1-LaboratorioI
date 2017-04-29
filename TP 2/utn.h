ab10d5537b64a6faf9d87b22815194a5f6fb00cd
perez-artaso <perez.artaso@gmail.com>
2017-04-28 22:47:42 -0300

    Add files via upload

---
+++ TP 2/utn.h
@@ -0,0 +1,15 @@
+/**
+* brief Valida que el texto ingresado contenga sólo letras.
+* param texto Es el array de caracteres que ingresará en la función.
+* return Devuelve un 1 en caso de que sean todas letras y un 0 en caso encontrar algún caracter distinto de una letra.
+*/
+
+int validarLetras(char texto[]);
+
+/**
+* brief Valida que el dato ingresado contenga sólo números.
+* param numero Es el dato que ingresará en la función.
+* return Devuelve un 1 en caso de que sean todos números y un 0 en caso encontrar algún caracter distinto de un número.
+*/
+
+int validarNumeros(char numero[]);
